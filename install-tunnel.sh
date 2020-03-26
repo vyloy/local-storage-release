47906744991018509c842ee7f4a394c94862af45
vyloy <vyloy.w@gmail.com>
2020-03-26 17:04:02 +0800

    init

---
+++ install-tunnel.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+NAME=tunnel
+DOWNLOAD_URL=https://github.com/vyloy/local-storage-release/raw/master
+DOWNLOAD_BINARY_URL=https://github.com/vyloy/local-storage-release/releases/download/v1.1.0
+CONFIG_PATH=/etc/${NAME}
+CONFIG_FILE="${CONFIG_PATH}/${NAME}.conf"
+SERVICE_PATH=/etc/systemd/system
+SERVICE_FILE="${SERVICE_PATH}/${NAME}.service"
+BIN_PATH=/usr/local/sbin
+BIN_FILE=${BIN_PATH}/${NAME}
+
+if [[ ! -s ${CONFIG_FILE} ]]; then
+ echo "installing config file to ${CONFIG_PATH}"
+ sudo mkdir -p ${CONFIG_PATH}
+ sudo chown root:wheel ${CONFIG_PATH}
+ sudo chmod 775 ${CONFIG_PATH}
+ sudo curl -o ${CONFIG_FILE} -fsSL "${DOWNLOAD_URL}/${NAME}.conf"
+else
+ echo "skip installing config file."
+fi
+
+echo "installing binary file to ${BIN_PATH}"
+sudo chown root:wheel ${BIN_PATH}
+sudo chmod 775 ${BIN_PATH}
+sudo curl -o "${BIN_FILE}" -fsSL "${DOWNLOAD_BINARY_URL}/${NAME}"
+sudo chmod 775 ${BIN_FILE}
+
+echo "installing service file to ${SERVICE_PATH}"
+sudo chown root:wheel ${SERVICE_PATH}
+sudo chmod 775 ${SERVICE_PATH}
+sudo curl -o "${SERVICE_FILE}" -fsSL "${DOWNLOAD_URL}/${NAME}.service"
+
+echo "cat ${CONFIG_FILE}"
+echo "=================================================="
+echo ""
+cat ${CONFIG_FILE}
+echo ""
+echo "=================================================="
+echo ""
+echo "Please edit config file ${CONFIG_FILE}."
+echo "And then sudo systemctl daemon-reload, sudo systemctl start ${NAME}."
